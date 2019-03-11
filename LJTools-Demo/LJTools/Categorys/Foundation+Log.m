5b30b50be2f784b5aab63e4c9150e908b7d4a824
lijie <994482177@qq.com>
2019-03-11 14:49:10 +0800

    。。

--- LJTools-Demo/LJTools/Categorys/Foundation+Log.m
+++ LJTools-Demo/LJTools/Categorys/Foundation+Log.m
@@ -24,6 +24,29 @@
     
     return str;
 }
+
+-(NSString *)description{
+    NSMutableString *str = [NSMutableString string];
+    
+    [str appendString:@"{\n"];
+    
+    // 遍历字典的所有键值对
+    [self enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+        [str appendFormat:@"\t%@ = %@,\n", key, obj];
+    }];
+    
+    [str appendString:@"}"];
+    
+    // 查出最后一个,的范围
+    NSRange range = [str rangeOfString:@"," options:NSBackwardsSearch];
+    if (range.length != 0) {
+        // 删掉最后一个,
+        [str deleteCharactersInRange:range];
+    }
+    
+    return str;
+}
+
 @end
 
 @implementation NSArray (Log)
@@ -49,4 +72,27 @@
     
     return str;
 }
+
+-(NSString *)description{
+    NSMutableString *str = [NSMutableString string];
+    
+    [str appendString:@"[\n"];
+    
+    // 遍历数组的所有元素
+    [self enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        [str appendFormat:@"%@,\n", obj];
+    }];
+    
+    [str appendString:@"]"];
+    
+    // 查出最后一个,的范围
+    NSRange range = [str rangeOfString:@"," options:NSBackwardsSearch];
+    if (range.length != 0) {
+        // 删掉最后一个,
+        [str deleteCharactersInRange:range];
+    }
+    
+    return str;
+}
+
 @end
